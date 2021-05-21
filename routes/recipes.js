656cb7c8bdf4da2301c73c648e07ddf30a070cc4
ssan9 <perrsy@gmail.com>
2021-05-21 18:12:22 -0400

    added logic

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,5 +1,37 @@
 var recipes = require('../recipes.json');
 var router = require('express').Router();
 
+const Recipes = {
+	getById: recipeId => {
+	    let { ids } = recipeId;
+	    const recipeList = recipes.filter(recipe => recipe.id == ids)
+	    return recipeList[0].ingredients;
+  	}
+}
+
+router.get('/shopping-list', (req, res) => {
+    const arr = []
+    const recipeId = recipes.forEach(recipe => {
+      if (recipe.id == req.query.ids) {
+        arr.push(recipe.id);
+      }
+    });
+
+    if (arr[0] == req.query.ids) {
+      if (req.query.ids) {
+        return res.json(Recipes.getById(req.query));
+      }
+      else {
+        res.status(400).end();
+      } 
+    }
+    else {
+      res.status(404).json({
+          errorMessage: 'NOT_FOUND'
+      });
+    }
+});
+
 module.exports = router;
+
 
